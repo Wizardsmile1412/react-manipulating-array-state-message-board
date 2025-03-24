f07f624c7ccfd31b2166e03d05267cc006f2de99
Tor <p.sripayom@gmail.com>
2025-03-24 14:23:07 +0700

    Assignment finished

--- src/components/MessageBoard.jsx
+++ src/components/MessageBoard.jsx
@@ -1,4 +1,28 @@
+import { useState } from "react";
+
+
 function MessageBoard() {
+const [textInput, setTextInput] = useState("");
+const [blog, setBlog] = useState([]);
+
+function handleTextInput(e) {
+  setTextInput(e.target.value);
+}
+
+function handleMsg(e) {
+  e.preventDefault()
+  const newblog = [...blog, textInput];
+  setBlog(newblog);
+  setTextInput("")
+}
+
+function handleDelete(indexToRemove){
+  // const newBlog = blog.filter((_,index)=> index !== indexToRemove);
+  // setBlog(newBlog);
+
+  setBlog((prevBlog) => prevBlog.filter((_, index) => index !== indexToRemove));
+}
+
   return (
     <div className="app-wrapper">
       <h1 class="app-title">Message board</h1>
@@ -9,16 +33,24 @@ function MessageBoard() {
             name="message-text"
             type="text"
             placeholder="Enter message here"
+            value={textInput}
+            onChange={handleTextInput}
           />
         </label>
-        <button className="submit-message-button">Submit</button>
+        <button className="submit-message-button" onClick={handleMsg}>Submit</button>
       </div>
+
       <div class="board">
-        <div className="message">
-          <h1>Hello all ! This is first message.</h1>
-          <button className="delete-button">x</button>
+        {blog.map((blogItem, index)=> {
+        return(
+        <div className="message" key={index}>
+          <h1>{blogItem}</h1>
+          <button className="delete-button" onClick={() => handleDelete(index)}
+          >x</button>
         </div>
+        )})}
       </div>
+
     </div>
   );
 }
